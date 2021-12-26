f46fb723a686250a98fd14d0e1cddaf8a4a0b9fe
yehtut <55129962+yehtutnaing@users.noreply.github.com>
2021-12-26 19:04:06 +0630

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,69 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "မလွမ်းပိုလေး မငိုတော.ဘူး",
+    artist: "မေကြေးမှူံ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "နေပါတော့",
+    artist: "မိုးထက်မြင့်",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Reflection",
+    artist: "Yoon Myat Thu",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "အကြိမ်ကြိမ်",
+    artist: "Yoon Myat Thu",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "နင်လမ်းတွေ.ခဲ့ရင်",
+    artist: "Yoon Myat Thu",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ဒွိဟ",
+    artist: "Yoon Myat Thu",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "နှစ်ယောက်တစ်အိမ်မက်",
+    artist: "Minn Khant",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name:"မိုး",
+    artist:"Minn khint",
+    img: "music-8",
+    src: "music-8",
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
